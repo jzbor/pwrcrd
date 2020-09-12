5e18ae8cabb13047b9ee1c15e4d3a8339034fdf2
jzbor <jzbor@posteo.net>
2020-09-12 18:04:39 +0200

    Adding output messages

--- src/main.py
+++ src/main.py
@@ -24,33 +24,31 @@ for f in formats:
     assert chord_format.valid_format(f), 'Bricked format: {}'.format(f)
 
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
 def save(exported_song, path, args):
     if args.output_path:
         if os.path.isfile(path) and not args.force:
-            print('WARNING: File {} already exists and will NOT be overwritten!'.format(path))
-            return False
+            eprint('WARNING: File {} already exists and will NOT be overwritten!'.format(path))
+            return False, path
         elif os.path.isdir(path):
             # create filename of format 'filename.end' or 'filename3.end'
-            filename = '{}{{}}.{}'.format(exported_song.get_filename(), exported_song.format['file_endings'][0])
-            if os.path.exists(os.path.join(path, filename.format(''))) and not args.force:
-                i = 1
-                while os.path.exists(os.path.join(path, filename.format(i))):
-                    i += 1
-                filename = filename.format(i)
-            else:
-                filename = filename.format('')
+            filename = '{}.{}'.format(exported_song.get_filename(), exported_song.format['file_endings'][0])
             path = os.path.join(path, filename)
-            file = open(path, 'w')
-            file.write(exported_song.content)
-            file.close()
-            return True
+            if os.path.exists(path) and not args.force:
+                eprint('WARNING: File {} already exists and will NOT be overwritten!'.format(path))
+                return False, path
+            with open(path, 'w') as file:
+                file.write(exported_song.content)
+            return True, path
         elif not os.path.isfile(path) or args.force:
-            file = open(path, 'w')
-            file.write(exported_song.content)
-            file.close()
-            return True
+            with open(path, 'w') as file:
+                file.write(exported_song.content)
+            return True, path
         else:
-            return False
+            return False, path
 
 
 def select_exporteur(identifier):
@@ -88,30 +86,38 @@ def parse_args():
                         nargs=1, dest='output_path')
     parser.add_argument('-f', '--force', help='overwrite existing files',
                         action='store_true')
-    # @TODO Following functions to be implemented
-    # * delete source files
-    # * verbosity
-    # * generate pdf (via latex or chord pro)
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
 
-    urls = args.source.split(',')
+    if not ',' in args.source and args.source.endswith('.list') and os.path.isfile(args.source):
+        with open(args.source, 'r') as f:
+            urls = f.readlines()
+        for i, url in enumerate(urls):
+            urls[i] = url.replace('\n', '')
+    else:
+        urls = args.source.split(',')
 
-    for url in urls:
+    url_count = len(urls)
+
+    for index, url in enumerate(urls):
         importeur = select_importeur(url)
         export_format = select_format(args.filetype)
+        eprint(f'({index + 1}/{url_count}) => {url}')
+        eprint(f'\t Importing from {url}')
         imported_song = importeur.load(url)
 
-        print(imported_song)
-        print(imported_song.meta)
-
+        eprint(f'\t Exporting to {export_format["identifier"]}')
         exported_song = song.ExportedSong.create(imported_song, export_format)
 
         if args.output_path:
-            save(exported_song, args.output_path[0], args)
+            success, path = save(exported_song, args.output_path[0], args)
+            if success:
+                eprint(f'\t Saved to {path}')
+            else:
+                eprint(f'\t Unable to save song to {path}')
         else:
             print(exported_song.content)
 
